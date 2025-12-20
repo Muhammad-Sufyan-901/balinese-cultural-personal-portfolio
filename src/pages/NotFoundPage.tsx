578eaa8174056a5958202e9bd7ff1ad06adb7bde
Muhammad Sufyan <108451771+Muhammad-Sufyan-901@users.noreply.github.com>
2025-12-20 18:34:54 +0800

    refactor : making the not found page is reusable

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -6,25 +6,34 @@ import { Text } from "@/components/common/Text";
 import { Link } from "@/components/common/Link";
 import { Button } from "@/components/ui/button";
 
-export default function NotFoundPage(): React.JSX.Element {
+type NotFoundPageProps = {
+  title?: string;
+  description?: string;
+  backButtonText?: string;
+  backButtonHref?: string;
+};
+
+export default function NotFoundPage({
+  title = "404 - Halaman Tidak Ditemukan",
+  description = "Halaman yang anda tuju tidak ditemukan, silahkan kembali ke halaman beranda melalui tombol dibawah",
+  backButtonText = "Kembali ke Beranda",
+  backButtonHref = "/",
+}: NotFoundPageProps): React.JSX.Element {
   return (
     <Box className="flex flex-col gap-y-6 items-center justify-center min-h-screen">
-      <Heading className="text-5xl text-primary font-black">
-        404 - Halaman Tidak Ditemukan
-      </Heading>
+      <Heading className="text-5xl text-primary font-black">{title}</Heading>
       <Text
         variant="default"
         className="text-center max-w-xl"
       >
-        Halaman yang anda tuju tidak ditemukan, silahkan kembali ke halaman
-        beranda melalui tombol dibawah
+        {description}
       </Text>
       <Button
         className="px-8 py-4"
         asChild
       >
-        <Link href="/">
-          Kembali ke Beranda <ArrowRight />
+        <Link href={backButtonHref}>
+          {backButtonText} <ArrowRight />
         </Link>
       </Button>
     </Box>
